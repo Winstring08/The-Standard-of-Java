59ef1c3766c105fe8c3a71ddd7a190b57c0df9c7
SeungHyeon <r46t8yh547@gmail.com>
2023-04-12 23:59:07 +0900

    Collectors

--- javaStandard/src/stream/CollectorEx.java
+++ javaStandard/src/stream/CollectorEx.java
@@ -6,30 +6,45 @@ import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
+class StudentMember { //name, id, pwd를 iv로 가지고 있는 클래스 생성
+	String name;
+	String id;
+	String pwd;
+	
+	public StudentMember(String name, String id, String pwd) {
+		super();
+		this.name = name;
+		this.id = id;
+		this.pwd = pwd;
+	}
+	
+	String getName() { //name값을 얻어올 메서드 생성
+		return name;
+	}
+	
+}
+
 public class CollectorEx {
 
 	public static void main(String[] args) {
 		
-		String[] stuArr = {"김자바","이자바","박자바","권자바","신자바"};
+		StudentMember[] stuArr = { //요소가 5개인 StudentMember배열 생성
+				new StudentMember("짱구","aaa","111"),
+				new StudentMember("유리","bbb","222"),
+				new StudentMember("훈이","ccc","333"),
+				new StudentMember("철수","ddd","444"),
+				new StudentMember("맹구","eee","555")
+		};
 		
-		Stream<String> stuStream = Stream.of(stuArr);
+		Stream<StudentMember> stuStream = Stream.of(stuArr);
+		//배열을 Stream으로 변환
 		
-		List<String> names = stuStream.collect(Collectors.toList());
+		List<String> stuList = stuStream.map(StudentMember::getName)
+				//각요소에 getName메서드 작업 실행한 리턴값들을 요소로 하는 Stream<String>반환
+				.collect(Collectors.toList());
+		//그 Stream<String>을 List로 변환
 		
-		ArrayList<String> list = names.stream().collect(
-				Collectors.toCollection(ArrayList::new)
-				);
 		
-		Map<String,String>map = stuStream.collect(
-				Collectors.toMap(p->p + "의 키", p->p + "의 값")
-				);
-		
-		Student[] stuName = stuStream.toArray(Student[]::new);
-		//Student[] stuName = stuStream.toArray(); 불가능
-		Object[] stuNames = stuStream.toArray();
-		
-		long count = stuStream.count();
-		count = stuStream.collect(Collectors.counting());
 	}
 
 }
